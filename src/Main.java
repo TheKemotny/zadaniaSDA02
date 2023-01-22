5cfd7e8d429c929c84c36c9f7404055ce5cf1643
Tomasz Szyerek <tomek.szyperek@gmail.com>
2023-01-22 06:19:57 +0100

    +pierwsze zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
+    public static void pierwszeZadanie(){
+        Random random = new Random();
+        int[] tab = new int[15];
+        for(int i = 0; i < tab.length; i++){
+            tab[i] = (random.nextInt(10)+1)*2;
+        }
+
+        for(int i = 0; i < tab.length; i++){
+            System.out.println("tab[" + i + "]: " + tab[i]);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        pierwszeZadanie();
     }
 }
